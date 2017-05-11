3bed3eccb5c5e0ad55ae5d09615a94770c934482
Jason Nestmann <nestmannj@bellsouth.net>
2017-05-11 17:40:28 -0400

    Added staff directory information

---
+++ faculty_staff_data.py
@@ -0,0 +1,27 @@
+
+img_location = 'images/faculty_staff/'
+
+people = [
+    {'name': 'Ka-Wing Wong', 'title': 'Dept. Chair', 'office': 'Wallace 418', 'email': 'Ka-Wing.wong@eku.edu', 'phone': '859-622-2398', img: 'wong.jpg'},
+    {'name': 'Ginger Gordon', 'title': 'Administrative Assistant', 'office': 'Wallace 417', 'email': 'ginger.gordon@eku.edu', 'phone': '859-622-2398', img: 'blank.jpg'},
+    {'name': 'Tom Kay', 'title': 'Laboratory Manager', 'office': 'Wallace 408', 'email': 'Tom.Kay@eku.edu', 'phone': '859-622-1924', img: 'wong.jpg'},
+    {'name': 'Cindy Bragg', 'title': 'Lecturer', 'office': 'Wallace 412', 'email': 'cindy.bragg@eku.edu', 'phone': '859-622-1874', img: 'wong.jpg'},
+    {'name': 'Kuang-Nan Chang', 'title': 'Professor', 'office': 'Wallace 409', 'email': 'Kuangnan.Chang@eku.edu', 'phone': '859-622-1935', img: 'wong.jpg'},
+    {'name': 'George V Landon', 'title': 'Associate Professor', 'office': 'Wallace 401', 'email': 'George.Landon@eku.edu', 'phone': '859-622-3184', img: 'wong.jpg'},
+    {'name': 'Chunchao Lane', 'title': 'Visiting Assistant Professor', 'office': 'Wallace 416', 'email': 'Chunchao.Lane@eku.edu', 'phone': '859-622-3492', img: 'wong.jpg'},
+    {'name': 'Chongkye Rhee', 'title': 'Professor', 'office': 'Wallace 413', 'email': 'Chongkye.Rhee@eku.edu', 'phone': '859-622-2104', img: 'wong.jpg'},
+    {'name': 'Eugene Fred Styer', 'title': 'Associate Professor', 'office': 'Wallace 404', 'email': 'Eugene.Styer@eku.edu', 'phone': '859-622-1903', img: 'wong.jpg'},
+    {'name': 'Mengkun Yang', 'title': 'Associate Professor', 'office': 'Wallace 411', 'email': 'mengkun.yang@eku.edu', 'phone': '859-622-1923', img: 'wong.jpg'},
+    {'name': 'Shuangteng Zhang', 'title': 'Professor', 'office': 'Wallace 415', 'email': 'Shuangteng.Zhang@eku.edu', 'phone': '859-622-1925', img: 'wong.jpg'}
+]
+
+
+for person in people:
+    print(person['name'])
+    print(person['title'])
+    print(person['office'])
+    print(person['email'])
+    print(person['phone'])
+
+    print('-'*40)
+
